8b38d5a04b6ebd63fa73b7208502a8ad3cf16b46
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-11-06 15:48:50 +0530

    Fix linter errors

--- src/fetch.js
+++ src/fetch.js
@@ -1,13 +1,13 @@
 const url = 'https://api.openweathermap.org/data/2.5/weather?q=';
-
+/* global config */
 const fetchWeather = async (city) => {
-	try{
-		const response = await fetch(`${url}${city}&appid=${config.apiKey}`);
-    	const information = await response.json();
-    	return information;
-	} catch (err) {
-	return err;
-	}
+  try {
+    const response = await fetch(`${url}${city}&appid=${config.apiKey}`);
+    const information = await response.json();
+    return information;
+  } catch (err) {
+    return err;
+  }
 };
 
 export default fetchWeather;
--- src/index.js
+++ src/index.js
@@ -1,36 +1,35 @@
-import fetchWeather from './fetch'
+import fetchWeather from './fetch';
 
 const newForm = document.querySelector('[data-new-form]');
 const newCityName = document.querySelector('[data-new-city-input]');
-const mainContainer= document.getElementById('container');
-
-const display = async (city) => {
-	clearElement(mainContainer);
-	const res = await fetchWeather(city);
-	const resultDiv = document.createElement('div');
-	const nameContainer = document.createElement('p');
-	const weatherTypeContainer = document.createElement('p');
-	const tempContainer = document.createElement('p');
-	nameContainer.innerText = `Name of the city: ${res.name}`;
-	weatherTypeContainer.innerText = `Weather type: ${res.weather[0].description}`;
-	tempContainer.innerText = `City Temperature in Fahrenheit: ${res.main.temp}`;
-	resultDiv.appendChild(nameContainer);
-	resultDiv.appendChild(weatherTypeContainer);
-	resultDiv.appendChild(tempContainer);
-	mainContainer.appendChild(resultDiv);
-
-}
-
-newForm.addEventListener('submit', e => {
-	e.preventDefault();
-	const city = newCityName.value;
-	if (city == null || city === '') return;
-	display(city);
-	newForm.reset();
-});
+const mainContainer = document.getElementById('container');
 
 const clearElement = (element) => {
-    while (element.firstChild) {
-      element.removeChild(element.firstChild);
-    }
-  };
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+};
+
+const display = async (city) => {
+  clearElement(mainContainer);
+  const res = await fetchWeather(city);
+  const resultDiv = document.createElement('div');
+  const nameContainer = document.createElement('p');
+  const weatherTypeContainer = document.createElement('p');
+  const tempContainer = document.createElement('p');
+  nameContainer.innerText = `Name of the city: ${res.name}`;
+  weatherTypeContainer.innerText = `Weather type: ${res.weather[0].description}`;
+  tempContainer.innerText = `City Temperature in Fahrenheit: ${res.main.temp}`;
+  resultDiv.appendChild(nameContainer);
+  resultDiv.appendChild(weatherTypeContainer);
+  resultDiv.appendChild(tempContainer);
+  mainContainer.appendChild(resultDiv);
+};
+
+newForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const city = newCityName.value;
+  if (city == null || city === '') return;
+  display(city);
+  newForm.reset();
+});
