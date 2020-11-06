a037ae76a870e46ad44618ebb2f047d5d4bf1012
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-11-06 20:29:23 +0530

    Add the change temprature type feature

--- src/domcontroller.js
+++ src/domcontroller.js
@@ -1,12 +1,19 @@
 const newForm = document.querySelector('[data-new-form]');
 const newCityName = document.querySelector('[data-new-city-input]');
 const mainContainer = document.getElementById('container');
-
+const tempTemplate = document.getElementById('temp-template');
 
 const resultDiv = document.createElement('div');
 const nameContainer = document.createElement('h1');
 const weatherTypeContainer = document.createElement('p');
 const tempContainer = document.createElement('p');
+const humidityContainer = document.createElement('p');
+const tempType = document.createElement('span');
+
+const temperatureSwitch = () => {
+  const tempElement = document.importNode(tempTemplate.content, true);
+  return tempElement;
+};
 
 export {
   newForm,
@@ -16,4 +23,7 @@ export {
   nameContainer,
   weatherTypeContainer,
   tempContainer,
+  humidityContainer,
+  temperatureSwitch,
+  tempType,
 };
--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ import {
   nameContainer,
   weatherTypeContainer,
   tempContainer,
+  humidityContainer,
+  temperatureSwitch,
+  tempType,
 } from './domcontroller';
+
 
 const clearElement = (element) => {
   while (element.firstChild) {
@@ -15,16 +19,45 @@ const clearElement = (element) => {
   }
 };
 
+const convertTemp = (val, type) => {
+  let temp = null;
+  if (type === ' ℃') {
+    temp = (val * (9 / 5) + 32).toFixed(2);
+    type = ' ℉';
+  } else {
+    temp = ((val - 32) * (5 / 9)).toFixed(2);
+    type = ' ℃';
+  }
+  return { temp, type };
+};
+
 const display = async (city) => {
   clearElement(mainContainer);
   const res = await fetchWeather(city);
   if (res.cod === 200) {
-    nameContainer.innerText = `City name: ${res.name}`;
+    const tswitch = temperatureSwitch();
+    mainContainer.appendChild(tswitch);
+    nameContainer.innerText = `Location: ${res.name}, ${res.sys.country}`;
     weatherTypeContainer.innerText = `Weather type: ${res.weather[0].description}`;
-    tempContainer.innerText = `Temperature in Fahrenheit: ${res.main.temp}`;
+    tempContainer.innerText = `Temperature: ${(res.main.temp) / 10}`;
+    humidityContainer.innerText = `Humidity: ${res.main.humidity} %`;
+    tempContainer.appendChild(tempType);
+    tempType.innerText = ' ℃';
+    const mainswitch = document.getElementById('mainSwitch');
+    mainswitch.innerText = 'convert in ℉';
+    const { temp } = res.main;
+    mainswitch.addEventListener('click', e => {
+      e.preventDefault();
+      mainswitch.innerText = `convert in ${tempType.innerText}`;
+      const switched = convertTemp(temp, tempType.innerText);
+      tempType.innerText = `${switched.type}`;
+      tempContainer.innerText = `Temperature: ${switched.temp}`;
+      tempContainer.appendChild(tempType);
+    });
 
     weatherTypeContainer.classList.add('city-name');
     tempContainer.classList.add('city-name');
+    humidityContainer.classList.add('city-name');
     if (res.weather[0].main === 'Rain') {
       document.body.className = 'rainy';
     } else if (res.weather[0].main === 'Haze') {
