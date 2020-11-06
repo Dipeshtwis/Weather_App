97456da8d015032dea1bbda02db45d41d3d8196d
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-11-06 23:42:14 +0530

    Fix the weather fetch

--- src/fetch.js
+++ src/fetch.js
@@ -1,5 +1,6 @@
 const url = 'https://api.openweathermap.org/data/2.5/weather?q=';
-/* global apikey */
+const apikey = '23f95263438cd05c860d3dc7f6c8bc0c';
+
 const fetchWeather = async (city) => {
   try {
     const response = await fetch(`${url}${city}&appid=${apikey}`);
